2c3d027e8c5d9f69296e4dc59716b8dd42a92092
seakmengc <c.seakmeng0603@gmail.com>
2024-10-02 13:45:52 +0700

    Updates

---
+++ precache-manifest.b41106dab36ecc31c1b46eb1b90b6062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0760c2b422a4246f5140b448bf859916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f981d02749ff8c55eb8",
+    "url": "/static/css/main.5f7174ba.chunk.css"
+  },
+  {
+    "revision": "9d8d732c91279e7516d5",
+    "url": "/static/js/2.2e81a5b9.chunk.js"
+  },
+  {
+    "revision": "4f981d02749ff8c55eb8",
+    "url": "/static/js/main.b8cfec49.chunk.js"
+  },
+  {
+    "revision": "2e079d345d5ab9efc445",
+    "url": "/static/js/runtime-main.dc7a2442.js"
+  }
+]);
